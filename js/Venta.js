db3e51029d1b0e015475003b1610f478daa29483
XojeHome <dawxoje@gmail.com>
2021-02-17 09:22:12 +0100

    Logica form ready, falta la funcionalidad, ahora arreglar POST

--- js/Venta.js
+++ js/Venta.js
@@ -1,11 +1,13 @@
 class Venta {
     static deckVentasWrapper = document.querySelector(".ventasDisplay");
     static formVentaWrapper = document.querySelector(".ventasForm");
-    static inputProducto=document.getElementById('venta_producto');
+    static inputProducto = document.getElementById('venta_producto');
+    static outputProducto = document.getElementById('output_producto');
     static inputCliente = document.getElementById('venta_cliente');
     static outputCliente = document.getElementById('output_cliente');
-    static btn_submit=document.getElementById('btn_submit');
-    
+    static btn_submit = document.getElementById('btn_submit');
+    static ventaOutput = document.getElementById('ventaOutput');
+
     productos = new Array();
     cliente = new Cliente({ nombre: "", apellidos: "", dni: "", fechaNac: "", email: "", contrasenya: "" })
 
@@ -82,16 +84,22 @@ class Venta {
         Venta.setClientes();
         Venta.setProductos();
         //bind producto
-        this.inputProducto.disabled=true;
-        this.btn_submit.disabled=true;
+        this.inputProducto.disabled = true;
+        this.btn_submit.disabled = true;
+        this.ventaOutput.style.display = 'none';
+        this.inputCliente.addEventListener('change', () => {
+            const cliente = this.inputCliente.value;
+            if (cliente != 'None') {
+                this.outputCliente.textContent = cliente;
+                this.inputCliente.disabled = true;
+                this.inputProducto.disabled = false;
+                this.ventaOutput.style.display = 'flex';
 
-        this.inputCliente.addEventListener('change',()=>{
-            const cliente=this.inputCliente.value;
-            if (cliente!='None') {
-                this.outputCliente.textContent=cliente;
-                this.inputCliente.disabled=true;
-                this.inputProducto.disabled=false;
-                this.btn_submit.disabled=false;
+            }
+        });
+        this.inputProducto.addEventListener('change', () => {
+            if (this.inputProducto.value != 'None') {
+                this.btn_submit.disabled = false;
             }
         });
         //form
@@ -102,12 +110,12 @@ class Venta {
         let newVenta;
         const formElements = Venta.formVentaWrapper.elements;
         Venta.formVentaWrapper.addEventListener("submit", (event) => {
-            const producto=document.getElementById('venta_producto').value;
-            let cantidad=0;
-            console.log(producto);
+            const producto = document.getElementById('venta_producto').value;
+            let cantidad = 0;
             do {
                 cantidad = parseInt(window.prompt("Please enter a number from 1 to 100", 1), 10);
             } while (isNaN(cantidad) || cantidad > 100 || cantidad < 1);
+            Venta.addProductoCarrito(cantidad);
             event.preventDefault();
         })
     }
@@ -151,6 +159,16 @@ class Venta {
             option.innerHTML = producto.referencia;
             inputProducto.appendChild(option);
         }
+    }
+
+    static addProductoCarrito(cantidad = 0) {
+        if (this.outputProducto.innerText == 'NONE') {
+            this.outputProducto.innerText = '';
+        }
+        this.outputProducto.innerHTML += `${this.inputProducto.value} x ${cantidad}
+                    <a class="btn btn-danger btn-sm " href="#" role="button">delete</a>
+        
+        `;
     }
 
 
